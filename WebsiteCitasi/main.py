594eabd458cb9b4f4b441c00da3a4b7995cdc55d
Murdangs <134845723+ardhien50@users.noreply.github.com>
2024-03-17 12:38:08 +0700

    Add files via upload

---
+++ WebsiteCitasi/main.py
@@ -0,0 +1,44 @@
+from asyncio import run
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+import account,kalkulator,metode,penjelasan
+st.set_page_config(
+        page_title="citasi",
+)
+
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self,title, func):
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run():
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='Citasi ',
+                options=['Account','Kalkulator','Metode','Penjelasan'],
+                icons=['person-circle','chat-text-fill','house-fill','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'grey'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "black"},
+        "nav-link-selected": {"background-color": "black"},}
+                
+                )
+
+        if app == "Account":
+            account.app()
+        if app == "Kalkulator":
+            kalkulator.app()
+        if app == "Metode":
+            metode.app()
+        if app == "Penjelasan":
+            penjelasan.app()
+    run()
